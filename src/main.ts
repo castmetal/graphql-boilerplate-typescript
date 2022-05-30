1f760e179075f8803524debfcf6aa6b1d2f64e7b
michel <michel.laguardia@sunoresearch.com.br>
2022-05-30 10:58:15 -0300

    chore(v2.0): update entire version

--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,50 @@
-import * as express from 'express';
-import * as session from 'express-session';
-import * as http from 'http';
-import { ApolloServer } from 'apollo-server-express';
-import apolloServerConfig from './apollo-server-config';
+import * as express from "express";
+import * as session from "express-session";
+import * as http from "http";
+import { ApolloServer } from "apollo-server-express";
+import apolloServerConfig from "./apollo-server-config";
+import { startTestServer } from "tests";
 
 // Import and init all models
 // import './models';
 
+async function startApolloServer() {
+  const app = express();
 
-const app = express();
+  app.use(
+    session({
+      resave: true,
+      saveUninitialized: true,
+      secret: "YOUR_VERY_SECRET_KEY_HERE",
+      cookie: {
+        secure: false,
+      },
+    })
+  );
 
-app.use(session({
-  resave: true,
-  saveUninitialized: true,
-  secret: 'YOUR_VERY_SECRET_KEY_HERE',
-  cookie: {
-    secure: false,
-  },
-}));
+  const apolloServer = new ApolloServer(apolloServerConfig);
 
-const apolloServer = new ApolloServer(apolloServerConfig);
+  await apolloServer.start();
 
-apolloServer.applyMiddleware({
-  app,
-  cors: {
-    origin: (origin, cb) => {
-      // TODO: Check if origin is allowed
-      cb(null, true);
+  apolloServer.applyMiddleware({
+    app,
+    cors: {
+      origin: (origin, cb) => {
+        // TODO: Check if origin is allowed
+        cb(null, true);
+      },
+      credentials: true,
     },
-    credentials: true,
-  },
-});
+  });
 
+  const httpServer = http.createServer(app);
 
-const httpServer = http.createServer(app);
-apolloServer.installSubscriptionHandlers(httpServer);
+  const PORT = process.env.PORT ?? 4000;
+  httpServer.listen(PORT, () => {
+    console.log(
+      `🚀 Server ready at http://localhost:${PORT}${apolloServer.graphqlPath}`
+    );
+  });
+}
 
-const PORT = 4000;
-httpServer.listen(PORT, () => {
-  console.log(`🚀 Server ready at http://localhost:${PORT}${apolloServer.graphqlPath}`);
-  console.log(`🚀 Subscriptions ready at ws://localhost:${PORT}${apolloServer.subscriptionsPath}`);
-});
+startApolloServer();
